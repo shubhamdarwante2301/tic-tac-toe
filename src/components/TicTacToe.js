6a6a36deee01aa2e46127d994b51a2d9772da399
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2022-09-22 13:16:19 +0530

    responsive

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,83 +1,108 @@
-import React, {useState, useEffect} from 'react';
-import './TicTacToe.css';
-/*this is my project*/
+import React, { useState, useEffect } from "react";
+import "./TicTacToe.css";
 
-let initialState = ['','','','','','','','',''];
-let turn = 'X';
+let initialState = ["", "", "", "", "", "", "", "", ""];
+let turn = "X";
 
 export default function TicTacToe() {
-    const[gameState, setGameState] = useState(initialState);
-    const[whosTurn, setWhosTurn] = useState(true);
-    const[winner, setWinner] = useState('');
+  const [gameState, setGameState] = useState(initialState);
+  const [whosTurn, setWhosTurn] = useState(true);
+  const [winner, setWinner] = useState("");
 
+  const play = (index) => {
+    turn = whosTurn ? "O" : "X";
+    let tempArr = Array.from(gameState);
+    if (tempArr[index] === "") {
+      tempArr[index] = whosTurn ? "X" : "O";
+      setWhosTurn(!whosTurn);
+      setGameState(tempArr);
+    } else {
+      alert("already played");
+    }
+  };
 
-    const play = (index) => {
-        turn = whosTurn ? 'O' : 'X';
-        let tempArr = Array.from(gameState);
-        if(tempArr[index] === '') 
+  useEffect(() => {
+      const modal = document.querySelector('#modal');
+      const resetBtn = document.querySelector('.reset-button');
+      if(winner !== "") {
+        modal.setAttribute('open',"open");
+        resetBtn.addEventListener('click', ()=>{
+            modal.close();
+            reset();
+        })
+      }
+  }, [winner])
+
+  useEffect(() => {
+    let tempArr = Array.from(gameState);
+
+    const arr = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < arr.length; i++) {
+      const [a, b, c] = arr[i];
+      if (
+        tempArr[a] &&
+        tempArr[a] === tempArr[b] &&
+        tempArr[b] === tempArr[c]
+      ) {
+        setWinner(`Player ${tempArr[a]} has won`);
+      }
+    }
+
+    let count = 0;
+    for(let i=0; i < tempArr.length; i++) {
+        if(tempArr[i] !== "")
         {
-            tempArr[index] = whosTurn ? 'X' : 'O';
-            setWhosTurn(!whosTurn);
-            setGameState(tempArr);
-            console.log(tempArr);
-        } else {
-            alert("already playd");
+            count++;
         }
-
+    }
+    if(count === tempArr.length)
+    {
+        setWinner("Game Tie");
     }
 
-    useEffect(() => {
-        let tempArr = Array.from (gameState);
-        //console.log(tempArr);
-        const arr = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-        let clearReset;
-        for(let i=0; i<arr.length; i++) {
-            const[a, b, c] = arr[i];
-            if(tempArr[a] && tempArr[a] === tempArr[b] && tempArr[b] === tempArr[c])
-            {
-                setWinner(tempArr[a]);
-                clearReset = setTimeout(()=>{
-                    reset();
-                },3000);
-                //console.log(i);
-                //console.log(tempArr[a]);
-            }
-        }
-        return () => {
-            clearTimeout(clearReset);
-        }
-    }, [gameState])
+  }, [gameState]);
 
-    const reset = () => {
-        setGameState(initialState);
-        setWinner('');
-    }
+  const reset = () => {
+    setGameState(initialState);
+    setWinner("");
+  };
 
-    return (
-        <div>
-            <h1 className="h1 app-name text-center m-2">Tic-Tac-Toe</h1>
-            <h3 className="app-name text-center">winner is {winner}</h3>
-            <h3 className="text-center"><button className="reset-button" onClick={reset}>Reset Game</button></h3>
-            <h2 className="app-name text-center ">{`Player ${turn} Turn`}</h2>
-            {/*<h2 className="app-name text-center">player X has Won</h2>*/}
-            <div className="game-board">
-                {
-                    gameState.map((data, index)=>{  
-                        return(
-                            <button key={index} className="item" onClick={()=>{play(index)}}><span className="text">{data}</span></button>
-                        )                      
-                    })
-                }
-            </div>
+  return (
+    <div>
+      <h1>Tic-Tac-Toe</h1>
+      <h2>{`Player ${turn} Turn`}</h2>
+      <dialog id="modal">
+        <h2>{winner}</h2>
+        <button className="reset-button">
+          Reset Game
+        </button>
+      </dialog>
+      <div className="main-container">
+        <div className="containers">
+          {gameState.map((data, index) => {
+            return (
+              <button
+                key={index}
+                className="item"
+                onClick={() => {
+                  play(index);
+                }}
+              >
+                <span className="text">{data}</span>
+              </button>
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
